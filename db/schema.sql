909af223d99fb44d542a250d076854bf17d97f6d
Kirby <legendofkirby85@gmail.com>
2018-12-11 16:37:01 -0800

    update - working on posts

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,6 @@ USE burgers_db;
 CREATE TABLE burgers (
 	id INT (10) AUTO_INCREMENT NOT NULL,
     burger_name VARCHAR (100) NOT NULL,
-    devoured BOOLEAN,
-    date TIMESTAMP,
+    devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
