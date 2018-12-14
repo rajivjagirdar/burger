3fc33180b02b71a478f2bc0a8694903f9cc0dddc
Kirby <legendofkirby85@gmail.com>
2018-12-13 16:22:59 -0800

    updated for JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
+  if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  }else {
+    connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "burgers_db"
+    });
+  };
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id: " + connection.threadId);
-});
+
+connection.connect();
 
 module.exports = connection;
